c736b7e8d19c0d425f9cd44e9b1dfe8f7659fd4b
Wangari-Sharon <wangarik35@gmail.com>
2022-07-22 14:00:29 +0300

    Done with Exercise 2

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -2,8 +2,34 @@ import styled from "styled-components";
 import React, { useEffect, useState } from "react";
 
 /* ✅ modify this usePokemon custom hook to take in a query as an argument */
-export function usePokemon() {
+export function usePokemon(query) {
   /* ✅ this hook should only return one thing: an object with the pokemon data */
+  const [ {data, status, errors}, setPokemon] = useState({
+    data:null,
+    status: 'idle',
+    errors: null
+  });
+  useEffect(() => {
+    setPokemon({ data: null, status: 'pending', errors: null} )
+    fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
+      .then((r) => {
+        if (r.ok) {
+          return r.json();
+        }
+        else {
+          return r.text().then((err) => {
+            throw err;
+          });
+        }
+      })
+      .then((pokemon) => {
+        setPokemon({data: pokemon, status: 'fulfilled', errors:null});
+      })
+      .catch((err) => {
+        setPokemon({data: null, status: 'rejected', errors: [err]})
+      })
+  }, [query])
+  return {data, status, errors}
 }
 
 function Pokemon({ query }) {
@@ -11,12 +37,15 @@ function Pokemon({ query }) {
    ✅ move the code from the useState and useEffect hooks into the usePokemon hook
    then, call the usePokemon hook to access the pokemon data in this component
   */
-  const [pokemon, setPokemon] = useState(null);
-  useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
-      .then(r => r.json())
-      .then(setPokemon);
-  }, [query]);
+  // const [pokemon, setPokemon] = useState(null);
+  // useEffect(() => {
+  //   fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
+  //     .then(r => r.json())
+  //     .then(setPokemon);
+  // }, [query]);
+  const {data: pokemon, status, errors} = usePokemon(query)
+
+  console.log ({pokemon, status, errors})
 
   // 🚫 don't worry about the code below here, you shouldn't have to touch it
   if (!pokemon) return <h3>Loading...</h3>;
@@ -60,7 +89,6 @@ const Wrapper = styled.section`
     Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   background: papayawhip;
   text-align: center;
-
   h1 {
     background: #ef5350;
     color: white;
@@ -70,7 +98,6 @@ const Wrapper = styled.section`
     color: white;
     font-size: 2rem;
   }
-
   form {
     display: grid;
     grid-template-columns: 1fr auto;
